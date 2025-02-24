76181a2392b5cf514273e4920ce3dc6572529844
EddyVillarrealMunoz <eddy.villarreal.munoz@est.una.ac.cr>
2025-02-23 21:31:50 -0600

    Implementación Test Div

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -24,4 +24,16 @@ class CalculatorTest {
     fun testMul(){
         assertEquals(15.0, calculator.multiply(5.0, 3.0))
     }
+
+    @Test
+    fun testDiv(){
+        assertEquals(2.0, calculator.divide(10.0, 5.0))
+    }
+
+    @Test
+    fun testDivByZero(){
+        assertThrows(IllegalArgumentException::class.java) {
+            calculator.divide(10.0, 0.0)
+        }
+    }
 }
