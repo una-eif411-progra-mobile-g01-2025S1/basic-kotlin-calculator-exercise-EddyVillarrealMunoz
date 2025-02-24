3564492ceb3972fb6ce25e2ee5c14395aaadef0a
EddyVillarrealMunoz <eddy.villarreal.munoz@est.una.ac.cr>
2025-02-23 18:30:30 -0600

    Definición e Implementación Clase Calculator

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,23 @@
+package org.example
+
+class Calculator {
+
+    fun add(a: Double, b: Double): Double {
+        return a + b
+    }
+
+    fun subtract(a: Double, b: Double): Double {
+        return a - b
+    }
+
+    fun multiply(a: Double, b: Double): Double {
+        return a * b
+    }
+
+    fun divide(a: Double, b: Double): Double {
+        if (b == 0.0) {
+            throw IllegalArgumentException("Division by zero is not allowed")
+        }
+        return a / b
+    }
+}
